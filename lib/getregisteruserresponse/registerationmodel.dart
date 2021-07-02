d4c607a512cf958352eb78afd3fc80466cab1e45
Pushpam Kumar <pushpam@quikieapps.com>
2021-07-02 19:50:40 +0530

    reg api completed

--- lib/getregisteruserresponse/registerationmodel.dart
+++ lib/getregisteruserresponse/registerationmodel.dart
@@ -1,13 +1,77 @@
+
 class RegistrationModel {
-  final int id;
-  final String title;
+  int? statusCode;
+String? message;
+  Payload? payload;
+  String?timeStamp;
 
-  RegistrationModel({required this.id, required this.title});
+  RegistrationModel({required this.statusCode, required this.message, required this.payload, required this.timeStamp});
 
-  factory RegistrationModel.fromJson(Map<String, dynamic> json) {
-    return RegistrationModel(
-      id: json['id'],
-      title: json['title'],
-    );
+  RegistrationModel.fromJson(Map<String, dynamic> json) {
+    statusCode = json['statusCode'];
+    message = json['message'];
+    payload =
+    (json['payload'] != null ? new Payload.fromJson(json['payload']) : null)!;
+    timeStamp = json['timeStamp'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['statusCode'] = this.statusCode;
+    data['message'] = this.message;
+    if (this.payload != null) {
+      data['payload'] = this.payload!.toJson();
+    }
+    data['timeStamp'] = this.timeStamp;
+    return data;
+  }
+}
+
+class Payload {
+  int? userId;
+  String? name;
+  String? phoneNumber;
+  String? userType;
+  String? email;
+  String? deviceId;
+  String? status;
+  String? imageUrl;
+  String? createdDate;
+
+  Payload(
+      {required this.userId,
+        required this.name,
+        required this.phoneNumber,
+        required this.userType,
+        required this.email,
+        required this.deviceId,
+        required this.status,
+        required this.imageUrl,
+        required this.createdDate});
+
+  Payload.fromJson(Map<String, dynamic> json) {
+    userId = json['userId'];
+    name = json['name'];
+    phoneNumber = json['phoneNumber'];
+    userType = json['userType'];
+    email = json['email'];
+    deviceId = json['deviceId'];
+    status = json['status'];
+    imageUrl = json['imageUrl'];
+    createdDate = json['createdDate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['userId'] = this.userId;
+    data['name'] = this.name;
+    data['phoneNumber'] = this.phoneNumber;
+    data['userType'] = this.userType;
+    data['email'] = this.email;
+    data['deviceId'] = this.deviceId;
+    data['status'] = this.status;
+    data['imageUrl'] = this.imageUrl;
+    data['createdDate'] = this.createdDate;
+    return data;
   }
 }
--- lib/getregisteruserresponse/registrationservices.dart
+++ lib/getregisteruserresponse/registrationservices.dart
@@ -4,19 +4,26 @@ import 'package:kisan_dost_app/getregisteruserresponse/registerationmodel.dart';
 
 Future<RegistrationModel> registerUser(String phoneNumber,String deviceId,String name,String email,String usertype) async {
   final response = await http.post(
-    Uri.parse('https://jsonplaceholder.typicode.com/albums'),
+    Uri.parse('http://kisandosth-elb-1255426508.ap-south-1.elb.amazonaws.com/user'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
-    body: jsonEncode(<String, String>{
-      'title': phoneNumber,
+    body: jsonEncode(<String, dynamic>{
+      "userId": null,
+      "name": name,
+      "phoneNumber": phoneNumber,
+      "userType": usertype,
+      "email": email, 
+      "deviceId": deviceId,
+      "status": null,
+      "createdDate": null
     }),
   );
 
-  if (response.statusCode == 201) {
+  if (response.statusCode == 200) {
     // If the server did return a 201 CREATED response,
     // then parse the JSON.
-    //  print("this is your response "+GetOtpModel.fromJson(jsonDecode(response.body)).message);
+    //  print("this is your response "+RegistrationModel.fromJson(jsonDecode(response.body)));
     return RegistrationModel.fromJson(jsonDecode(response.body));
   } else {
     // If the server did not return a 201 CREATED response,
